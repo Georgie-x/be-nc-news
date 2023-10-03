258706a3575a561d5e08fd61ef8a91420fd0b63e
Georgie-x <Georgiex@gmail.com>
2023-10-03 11:19:23 +0100

    Q3 complete

---
+++ controllers/api-controller.js
@@ -0,0 +1,9 @@
+const endpoints = require("../endpoints.json")
+
+exports.getEndpoints = async (req, res, next) => {
+    try{
+        return res.status(200).send({ endpoints })
+    } catch {
+        next(err)
+    }
+}
